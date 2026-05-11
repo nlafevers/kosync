db9b106b17645e7c890bbcaba03250caed28c5cf
nlafevers <nathan.lafevers@gmail.com>
2026-05-11 01:32:03 -0400

    feat: implement CLI for user management

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
@@ -11,6 +13,13 @@ import (
 
 func main() {
 	config := LoadConfig()
+
+	// Handle CLI commands
+	if len(os.Args) > 1 {
+		runCLI(config)
+		return
+	}
+
 	InitLogger(config.LogLevel)
 
 	slog.Info("KOSYNC starting",
@@ -67,4 +76,38 @@ func main() {
 		os.Exit(1)
 	}
 	slog.Info("server exited cleanly")
+}
+
+func runCLI(config Config) {
+	createCmd := flag.NewFlagSet("create-user", flag.ExitOnError)
+	createUsername := createCmd.String("username", "", "Username")
+	createPassword := createCmd.String("password", "", "Password")
+
+	deleteCmd := flag.NewFlagSet("delete-user", flag.ExitOnError)
+	deleteUsername := deleteCmd.String("username", "", "Username")
+
+	switch os.Args[1] {
+	case "create-user":
+		createCmd.Parse(os.Args[2:])
+		storage, _ := InitDB(config.DBPath)
+		defer storage.Close()
+		hash, _ := HashPassword(*createPassword)
+		if err := storage.CreateUser(*createUsername, hash); err != nil {
+			fmt.Printf("Error: %v\n", err)
+		} else {
+			fmt.Println("User created successfully")
+		}
+	case "delete-user":
+		deleteCmd.Parse(os.Args[2:])
+		storage, _ := InitDB(config.DBPath)
+		defer storage.Close()
+		if err := storage.DeleteUser(*deleteUsername); err != nil {
+			fmt.Printf("Error: %v\n", err)
+		} else {
+			fmt.Println("User deleted successfully")
+		}
+	default:
+		fmt.Println("Unknown command")
+		os.Exit(1)
+	}
 }
